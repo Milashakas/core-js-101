8e14cb2ead4058dc10b55a2664c1920c73d43387
Milana Kasumava <milanamamedova52@mail.ru>
2023-05-30 20:20:06 +0300

    feat:finish task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,9 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let arr = new Array(len);
+  return arr.fill(undefined).map((item, index) => index + index + 1)
 }
 
 
