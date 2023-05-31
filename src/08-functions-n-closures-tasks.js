71afee5bc673b7718245a9d8ad064445469b9986
Milana Kasumava <milanamamedova52@mail.ru>
2023-05-31 10:21:35 +0300

    feat:finish some tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -172,8 +172,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom;
+  return function b() {
+    current += 1;
+    return current;
+  };
 }
 
 
